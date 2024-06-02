7939791ec860bf2486d3cd0e19108b5eb95031aa
rob.bennell <rob.bennell@accenture.com>
2024-06-02 22:05:13 +0100

    Add mgmt command

--- backend/buildings/management/commands/import_readings.py
+++ backend/buildings/management/commands/import_readings.py
@@ -1,14 +1,36 @@
-from django.core.management.base import BaseCommand, CommandError
+from typing import Dict, List
+from django.core.management.base import BaseCommand
 from buildings.models import Building, Reading
 import csv
+from django.db import transaction
 
 
 def read_file(building_name: str):
     filename = f"buildings_data/{building_name}.csv"
+    data = []
+    print("reading data!")
     with open(filename, "r") as inf:
-        csv_reader = csv.reader(inf)
+        csv_reader = csv.DictReader(inf)
         for r in csv_reader:
-            print(r)
+            data.append(r)
+    print(data)
+    return data
+
+
+@transaction.atomic  # this ensures if anything fails we don't save anything to the db!
+def add_data_for_building(building_name: str, data: List[Dict]):
+    print("creating building!")
+    building = Building.objects.create(name=building_name)
+    print("creating readings")
+    Reading.objects.bulk_create(
+        [
+            Reading(
+                building=building, date=row["date"], reading=float(row["reading"][:-3])
+            )
+            for row in data
+        ]
+    )
+    print(f"finished adding building {building_name}")
 
 
 class Command(BaseCommand):
@@ -19,5 +41,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         building_name = options["building_name"]
-        read_file(building_name)
-        # Building.objects.create(name=building_name)
+        data = read_file(building_name)
+        add_data_for_building(building_name, data)
